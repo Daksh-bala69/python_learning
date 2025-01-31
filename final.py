9bb1875c36be2997705f98df99f973ff31b9445f
Daksh-bala69 <dakshnonjuvenile@gmail.com>
2025-01-31 22:46:57 +0530

    finally counting

--- final.py
+++ final.py
@@ -4,35 +4,30 @@ import cv2
 
 from ultralytics import YOLO
 model=YOLO("C:\\Users\\BEAST SSJ3\\Downloads\\best.pt")
-results=model.predict(r"C:\Users\BEAST SSJ3\Downloads\UAS_DTU_Round_2_Task_data\UAS_DTU_Round_2_Task_data\8\imageuptodown_img_7_4.jpg",save=False)
+results=model.predict(r"C:\Users\BEAST SSJ3\Downloads\UAS_DTU_Round_2_Task_data\UAS_DTU_Round_2_Task_data\6\images_close_img_21_2.jpg",save=False)
 
 for result in results:
     print(result.boxes.numpy)
 
-count_for0=0
 count_for1=0
 count_for2=0
 count_for3=0
-count_for4=0
-count_for5=0
 
-for i in range(3):
-    if(result.boxes.cls[int(i)]==0):
-        count_for0+=1
-    if(result.boxes.cls[int(i)]==1):
+arr=result.boxes.cls.cpu().numpy()
+
+print(arr)
+
+for i in range(arr.size):
+    if(arr[i]==1):
         count_for1+=1
-    if(result.boxes.cls[int(i)]==2):
+    if(arr[i]==2):
         count_for2+=1
-    if(result.boxes.cls[int(i)]==3):
+    if(arr[i]==3):
         count_for3+=1
-    if(result.boxes.cls[int(i)]==4):
-        count_for4+=1
-    if(result.boxes.cls[int(i)]==5):
-        count_for5+=1
 
-print(count_for0)
+print(arr.size)
+
+
 print(count_for1)
 print(count_for2)
 print(count_for3)
-print(count_for4)
-print(count_for5)
